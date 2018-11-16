d739b0e522256dc9df332428204736d1d006fc19
Will J <wjjackson@Wills-iMac.local>
2018-11-15 16:55:05 -0800

    started work on actual xcode app

---
+++ Memelify/Memelify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memelify
+//
+//  Created by Will J on 11/15/18.
+//  Copyright © 2018 Memelify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
