835d5424590ca51b8cfbb0a08b69566e775645ba
Kauana dos Santos <kau.grola@gmail.com>
2018-11-22 16:46:50 -0800

    fix memetile bugs

--- Memelify/Memelify/Supports/MemeTile.swift
+++ Memelify/Memelify/Supports/MemeTile.swift
@@ -10,36 +10,14 @@ import UIKit
 
 class MemeTile: UITableViewCell {
 
+    @IBOutlet weak var meme: UIImageView!
     @IBOutlet weak var karma: UILabel!
     @IBOutlet weak var share: UIButton!
     @IBOutlet weak var favorite: UIButton!
+    @IBOutlet weak var buttons: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    func displayImage(address: String) {
-        if let url = URL(string: address) {
-            imageView?.contentMode = .scaleAspectFit
-            downloadImage(from: url)
-        }
-    }
-
-    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
-    }
-
-    func downloadImage(from url: URL) {
-        print("Download Started")
-        getData(from: url) { data, response, error in
-            guard let data = data, error == nil else { return }
-            print(response?.suggestedFilename ?? url.lastPathComponent)
-            print("Download Finished")
-            DispatchQueue.main.async() {
-                self.imageView?.image = UIImage(data: data)
-            }
-        }
-    }
-
 }
--- Memelify/Memelify/ViewControllers/HomeViewController.swift
+++ Memelify/Memelify/ViewControllers/HomeViewController.swift
@@ -8,24 +8,47 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var memeTable: UITableView!
 
+    var memes = [UIImage]()
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return memes.count
     }
-    
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let currentImage = memes[indexPath.row]
+        let ratio = currentImage.cropRatio()
+        return (tableView.frame.width / ratio) + 40
+    }
+
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tile = MemeTile()
-        tile.displayImage(address: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg")
-        return tile
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTilePrototype", for: indexPath) as! MemeTile
+        cell.meme.image = memes[indexPath.row]
+        return cell
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         memeTable.dataSource = self
-        // Do any additional setup after loading the view, typically from a nib.
+        memeTable.delegate = self
+
+        // https://memelify.herokuapp.com/api/memes/latest
+        if let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg") {
+            getData(from: url) { data, response, error in
+                guard let data = data, error == nil else { return }
+                DispatchQueue.main.async() {
+                    self.memes.append(UIImage(data: data)!)
+                    self.memeTable.reloadData()
+                }
+            }
+        }
     }
 
     // Shows Memelify logo on the navigation bar
@@ -36,6 +59,11 @@ class HomeViewController: UIViewController, UITableViewDataSource {
         imageView.image = UIImage(named: "Memelify-transparent.png")
         navigationItem.titleView = imageView
     }
-
 }
 
+extension UIImage {
+    func cropRatio() -> CGFloat {
+        let widthRatio = CGFloat(self.size.width / self.size.height)
+        return widthRatio
+    }
+}
