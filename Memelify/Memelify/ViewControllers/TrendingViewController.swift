6d1ec152d6f434cf829df2a275701651e87ed6ca
David Garwood <wjjackson7@gmail.com>
2018-11-24 23:18:12 -0800

    added the trending view (though it seems the same)

--- Memelify/Memelify/ViewControllers/TrendingViewController.swift
+++ Memelify/Memelify/ViewControllers/TrendingViewController.swift
@@ -7,20 +7,86 @@
 //
 
 import UIKit
+import Alamofire
 
-class TrendingViewController: UIViewController {
-
+class TrendingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    private let apiServer = "https://memelify.herokuapp.com/api/memes/hot"
+    
+    @IBOutlet weak var memeTable: UITableView!
+    
+    
+    var memes = [MemeObject]()
+    var favorites = [MemeObject]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let currentImage = memes[indexPath.row].image
+        let ratio = currentImage!.cropRatio()
+        return (tableView.frame.width / ratio) + 40
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTilePrototype", for: indexPath) as! MemeTile
+        cell.obj = memes[indexPath.row]
+        cell.meme.image = cell.obj?.image
+        cell.karma.text = "Karma: " + String(cell.obj?.likes ?? 0)
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        memeTable.dataSource = self
+        memeTable.delegate = self
+        
+        //save the favorite memes
+        //TODO: need to make sure this runs only ever once
+        let encodedData = NSKeyedArchiver.archivedData(withRootObject: self.favorites)
+        UserDefaults.standard.set(encodedData, forKey: "saved")
+        
+        
+        Alamofire.request(apiServer).responseJSON { response in
+            if let json = response.result.value as? [String: Any] {
+                guard let memes = json["memes"] as? [[String: Any]] else {
+                    return
+                }
+                
+                for meme in memes {
+                    guard let url = URL(string: meme["url"] as! String) else {
+                        continue
+                    }
+                    let id = meme["id"] as? String
+                    let date = meme["created"] as? String
+                    let title = meme["title"] as? String
+                    let likes = meme["likes"] as? Int
+                    
+                    self.getData(from: url) { data, response, error in
+                        guard let data = data, error == nil else { return }
+                        DispatchQueue.main.async() {
+                            let newMeme = MemeObject(id: id!,created: date!,title: title!,likes: likes!,pic: data)
+                            self.memes.append(newMeme)
+                            self.memeTable.reloadData()
+                        }
+                    }
+                }
+                
+            }
+        }
+        
     }
-
+    
     // Shows Memelify logo on the navigation bar
     override func viewDidAppear(_ animated: Bool) {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         imageView.contentMode = .scaleAspectFit
-
+        
         imageView.image = UIImage(named: "Memelify-transparent.png")
         navigationItem.titleView = imageView
     }
