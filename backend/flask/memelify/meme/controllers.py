ea9190555c02640722c6744d54ff78f6960c0f0d
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-12-05 02:33:18 -0800

    Added Filter to remove low funy score photos

--- backend/flask/memelify/meme/controllers.py
+++ backend/flask/memelify/meme/controllers.py
@@ -1,8 +1,13 @@
 """
 Handle all Meme requests
 """
+import datetime
+from datetime import date
+from sqlalchemy import Date, cast
+
 from flask import Blueprint, request, jsonify
 from memelify.meme.models import RedditMeme
+
 blueprint = Blueprint('meme',  __name__)
 
 
@@ -11,6 +16,7 @@ def query_latest_memes():
     offset = request.args.get('offset', default=0, type=int)
     limit = request.args.get('limit', default=10, type=int)
     latest_memes = (RedditMeme.query
+                    .filter(RedditMeme.funny_score > 0.2)  # 20% Threshold
                     .order_by(RedditMeme.created_utc.desc())
                     .offset(offset)
                     .limit(limit)
@@ -22,8 +28,20 @@ def query_latest_memes():
 def query_hottest_memes():
     offset = request.args.get('offset', default=0, type=int)
     limit = request.args.get('limit', default=10, type=int)
-    hottest_memes = (RedditMeme.query.order_by(RedditMeme.hotness.desc())
+    hottest_memes = (RedditMeme.query
+                     .filter(RedditMeme.funny_score > 0.2)  # 20% Threshold
+                     .order_by(RedditMeme.hotness.desc())
                      .offset(offset)
                      .limit(limit)
                      .all())
     return jsonify(memes=[meme.serialize for meme in hottest_memes])
+
+
+@blueprint.route('memes/top', methods=('GET', ))
+def get_top_meme():
+    since_date = date.today() - datetime.timedelta(7)
+    top_meme = (RedditMeme.query
+                .filter(cast(RedditMeme.created_utc, Date) < since_date)
+                .order_by(RedditMeme.hotness.desc())
+                .first())    
+    return jsonify(top_meme.serialize if top_meme else {})
